5b748cc8a24ab4a23c9bc68f0ad90c139283fe5b
diosmosis <diosmosis@users.noreply.github.com>
2021-05-18 13:51:43 -0700

    allowed_classes in unserialize

--- lib/Doctrine/Common/Cache/FilesystemCache.php
+++ lib/Doctrine/Common/Cache/FilesystemCache.php
@@ -68,7 +68,7 @@ class FilesystemCache extends FileCache
 
         fclose($resource);
 
-        return unserialize($data);
+        return unserialize($data, ['allowed_classes' => false]);
     }
 
     /**
